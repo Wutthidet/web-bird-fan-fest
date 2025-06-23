fd38a15378e6aa41beb6f85b4609e78f1175e714
Card <92069776+Wutthidet@users.noreply.github.com>
2025-06-23 09:55:05 +0700

    Update_2

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.14.167:8000/api',
+  apiUrl: 'http://43.209.102.40:3000/api',
   imgbb: {
     apiUrl: 'https://api.imgbb.com/1/upload',
     apiKey: '62e768cda1cad5a0b7e3930058a3a561'
