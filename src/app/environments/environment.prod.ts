0951d46b3998f2450bb569646444dc68b353aa69
Card <92069776+Wutthidet@users.noreply.github.com>
2025-06-23 09:42:07 +0700

    Update environment.prod.ts

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.birdfanfestival.com/api',
+  apiUrl: 'http://43.209.102.40:3000/api',
   imgbb: {
     apiUrl: 'https://api.imgbb.com/1/upload',
     apiKey: '62e768cda1cad5a0b7e3930058a3a561'
